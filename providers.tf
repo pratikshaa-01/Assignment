a940d9615a5fd8066001ddb7ef0cf042d25eda58
Pratiksha <pratu0104@gmail.com>
2024-11-26 19:24:12 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAUX6CPNVOKUNGC"
-  secret_key = "v5ovr2tNbAhmjcqIGwyj8HI7SDd0dn/tQtK"
+  access_key = "enter yours"
+  secret_key = "enter yours"
 }
